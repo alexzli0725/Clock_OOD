000508194cc25659b4a756ce56c08c14cbafd23f
alexzli0725 <151705696+alexzli0725@users.noreply.github.com>
2026-01-15 23:59:47 -0800

    real-time

--- Solution.java
+++ Solution.java
@@ -1,14 +1,19 @@
 public class Solution {
 
-    public static void main(String[] arg) {
+    public static void main(String[] arg) throws InterruptedException {
         Hour h = new Hour();
         Minute m = new Minute(h);
         Second s = new Second(m);
 
-        for (int i = 0; i < 86400; i++) {
+        // for (int i = 0; i < 86400; i++) {
+        while (true) {
             s.move();
-        }
+            Thread.sleep(1000); // wait 1 real second
 
-        System.out.println(h.getValue() + ":" + m.getValue() + ":" + s.getValue());
+            System.out.println(h.getValue() + ":" + m.getValue() + ":" + s.getValue());
+        }
+        // }
+
+        // System.out.println(h.getValue() + ":" + m.getValue() + ":" + s.getValue());
     }
 }
